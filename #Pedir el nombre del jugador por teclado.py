f7fbc1cc4c49b67c2a0860126da5725fcbdd4360
Salome Murcia <salomuu1225.02@gmail.com>
2023-07-03 07:30:36 -0500

    Create #Pedir el nombre del jugador por teclado.py
    Se inician los primeros cambios en el archivo

---
+++ #Pedir el nombre del jugador por teclado.py
@@ -0,0 +1,4 @@
+#Pedir el nombre del jugador por teclado
+#Imprimir un mensaje de bienvenida con el nombre del jugador
+nombreJugador = input("Ingresa tu nombre: ")
+print("Bienvenido, + nombreJugador + Disfruta tu juego.")
