059a06c3f1da8425b815b2e49a795ca26bb78fbc
Salome Murcia <salomuu1225.02@gmail.com>
2023-07-05 18:10:58 -0500

    corrección nombre del archivo y nombre de jugador

---
+++ Proyecto Integrador.py
@@ -0,0 +1,4 @@
+#Pedir el nombre del jugador por teclado
+#Imprimir un mensaje de bienvenida con el nombre del jugador
+nombre_jugador = input("Ingresa tu nombre: ")
+print("Bienvenido," + nombre_jugador + "\nDisfruta la partida!")
